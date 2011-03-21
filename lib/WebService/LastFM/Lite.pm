2e6216a91340c4ba11cc51947d4a945b159291dd
Toru, Kobayashi <koba0004@gmail.com>
2011-03-21 14:48:03 +0900

    initial commit

---
+++ lib/WebService/LastFM/Lite.pm
@@ -0,0 +1,32 @@
+package WebService::LastFM::Lite;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+WebService::LastFM::Lite -
+
+=head1 SYNOPSIS
+
+  use WebService::LastFM::Lite;
+
+=head1 DESCRIPTION
+
+WebService::LastFM::Lite is
+
+=head1 AUTHOR
+
+koba04 E<lt>koba0004@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
